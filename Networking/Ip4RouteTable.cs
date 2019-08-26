08c717d6945c34675f81ef9dab16fafea681173c
CodeCowboyOrg <CodeCowboyOrg@gmail.com>
2019-08-25 20:18:55 -0400

    Refactor to better encapsulate the native Win32 stuff.  Moved the Win32 API Structures into the Native class, renamed the NativeMethods class not Native

--- Networking/Ip4RouteTable.cs
+++ Networking/Ip4RouteTable.cs
@@ -8,60 +8,17 @@ namespace NetworkRoute
 {
     public class Ip4RouteTable
     {
-        [ComVisible(false), StructLayout(LayoutKind.Sequential)]
-        internal struct IPForwardTable
-        {
-            public uint Size;
-
-            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)]
-            public PMIB_IPFORWARDROW[] Table;
-        };
-
-        [ComVisible(false), StructLayout(LayoutKind.Sequential)]
-        internal struct PMIB_IPFORWARDROW
-        {
-            internal uint /*DWORD*/ dwForwardDest;
-            internal uint /*DWORD*/ dwForwardMask;
-            internal uint /*DWORD*/ dwForwardPolicy;
-            internal uint /*DWORD*/ dwForwardNextHop;
-            internal uint /*DWORD*/ dwForwardIfIndex;
-            internal uint /*DWORD*/ dwForwardType;
-            internal uint /*DWORD*/ dwForwardProto;
-            internal uint /*DWORD*/ dwForwardAge;
-            internal uint /*DWORD*/ dwForwardNextHopAS;
-            internal uint /*DWORD*/ dwForwardMetric1;
-            internal uint /*DWORD*/ dwForwardMetric2;
-            internal uint /*DWORD*/ dwForwardMetric3;
-            internal uint /*DWORD*/ dwForwardMetric4;
-            internal uint /*DWORD*/ dwForwardMetric5;
-        };
-
-        static IPForwardTable ReadIPForwardTable(IntPtr tablePtr)
-        {
-            var result = (IPForwardTable)Marshal.PtrToStructure(tablePtr, typeof(IPForwardTable));
-
-            PMIB_IPFORWARDROW[] table = new PMIB_IPFORWARDROW[result.Size];
-            IntPtr p = new IntPtr(tablePtr.ToInt64() + Marshal.SizeOf(result.Size));
-            for (int i = 0; i < result.Size; ++i)
-            {
-                table[i] = (PMIB_IPFORWARDROW)Marshal.PtrToStructure(p, typeof(PMIB_IPFORWARDROW));
-                p = new IntPtr(p.ToInt64() + Marshal.SizeOf(typeof(PMIB_IPFORWARDROW)));
-            }
-            result.Table = table;
-
-            return result;
-        }
 
         public static void RoutePrint(bool testing)
         {
             var fwdTable = IntPtr.Zero;
             int size = 0;
-            var result = NativeMethods.GetIpForwardTable(fwdTable, ref size, true);
+            var result = Native.GetIpForwardTable(fwdTable, ref size, true);
             fwdTable = Marshal.AllocHGlobal(size);
 
-            result = NativeMethods.GetIpForwardTable(fwdTable, ref size, true);
+            result = Native.GetIpForwardTable(fwdTable, ref size, true);
 
-            var forwardTable = ReadIPForwardTable(fwdTable);
+            var forwardTable = Native.ReadIPForwardTable(fwdTable);
 
             Marshal.FreeHGlobal(fwdTable);
 
@@ -103,12 +60,12 @@ namespace NetworkRoute
         {
             var fwdTable = IntPtr.Zero;
             int size = 0;
-            var result = NativeMethods.GetIpForwardTable(fwdTable, ref size, true);
+            var result = Native.GetIpForwardTable(fwdTable, ref size, true);
             fwdTable = Marshal.AllocHGlobal(size);
 
-            result = NativeMethods.GetIpForwardTable(fwdTable, ref size, true);
+            result = Native.GetIpForwardTable(fwdTable, ref size, true);
 
-            var forwardTable = ReadIPForwardTable(fwdTable);
+            var forwardTable = Native.ReadIPForwardTable(fwdTable);
 
             Marshal.FreeHGlobal(fwdTable);
 
@@ -158,7 +115,7 @@ namespace NetworkRoute
         public static void CreateRoute(Ip4RouteEntry routeEntry)
         {
 
-            var route = new PMIB_IPFORWARDROW
+            var route = new Native.MIB_IPFORWARDROW
             {
                 dwForwardDest = BitConverter.ToUInt32(IPAddress.Parse(routeEntry.DestinationIP.ToString()).GetAddressBytes(), 0),
                 dwForwardMask = BitConverter.ToUInt32(IPAddress.Parse(routeEntry.SubnetMask.ToString()).GetAddressBytes(), 0),
@@ -170,11 +127,11 @@ namespace NetworkRoute
                 dwForwardIfIndex = Convert.ToUInt32(routeEntry.InterfaceIndex)
             };
 
-            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(PMIB_IPFORWARDROW)));
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(Native.MIB_IPFORWARDROW)));
             try
             {
                 Marshal.StructureToPtr(route, ptr, false);
-                var status = NativeMethods.CreateIpForwardEntry(ptr);
+                var status = Native.CreateIpForwardEntry(ptr);
             }
             finally
             {
@@ -186,7 +143,7 @@ namespace NetworkRoute
         public static void CreateRoute(string destination, string mask, int interfaceIndex, int metric)
         {
             NetworkAdaptor adaptor = NicInterface.GetNetworkAdaptor(interfaceIndex);
-            var route = new PMIB_IPFORWARDROW
+            var route = new Native.MIB_IPFORWARDROW
             {
                 dwForwardDest = BitConverter.ToUInt32(IPAddress.Parse(destination).GetAddressBytes(), 0),
                 dwForwardMask = BitConverter.ToUInt32(IPAddress.Parse(mask).GetAddressBytes(), 0),
@@ -198,11 +155,11 @@ namespace NetworkRoute
                 dwForwardIfIndex = Convert.ToUInt32(interfaceIndex)
             };
 
-            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(PMIB_IPFORWARDROW)));
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(Native.MIB_IPFORWARDROW)));
             try
             {
                 Marshal.StructureToPtr(route, ptr, false);
-                var status = NativeMethods.CreateIpForwardEntry(ptr);
+                var status = Native.CreateIpForwardEntry(ptr);
             }
             finally
             {
@@ -214,7 +171,7 @@ namespace NetworkRoute
         public static void DeleteRoute(Ip4RouteEntry routeEntry)
         {
 
-            var route = new PMIB_IPFORWARDROW
+            var route = new Native.MIB_IPFORWARDROW
             {
                 dwForwardDest = BitConverter.ToUInt32(IPAddress.Parse(routeEntry.DestinationIP.ToString()).GetAddressBytes(), 0),
                 dwForwardMask = BitConverter.ToUInt32(IPAddress.Parse(routeEntry.SubnetMask.ToString()).GetAddressBytes(), 0),
@@ -226,11 +183,11 @@ namespace NetworkRoute
                 dwForwardIfIndex = Convert.ToUInt32(routeEntry.InterfaceIndex)
             };
 
-            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(PMIB_IPFORWARDROW)));
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(Native.MIB_IPFORWARDROW)));
             try
             {
                 Marshal.StructureToPtr(route, ptr, false);
-                var status = NativeMethods.DeleteIpForwardEntry(ptr);
+                var status = Native.DeleteIpForwardEntry(ptr);
             }
             finally
             {
@@ -269,18 +226,18 @@ namespace NetworkRoute
 
             var fwdTable = IntPtr.Zero;
             int size = 0;
-            var result = NativeMethods.GetIpForwardTable(fwdTable, ref size, true);
+            var result = Native.GetIpForwardTable(fwdTable, ref size, true);
             fwdTable = Marshal.AllocHGlobal(size);
 
-            result = NativeMethods.GetIpForwardTable(fwdTable, ref size, true);
+            result = Native.GetIpForwardTable(fwdTable, ref size, true);
 
-            var forwardTable = ReadIPForwardTable(fwdTable);
+            var forwardTable = Native.ReadIPForwardTable(fwdTable);
 
             Marshal.FreeHGlobal(fwdTable);
 
 
 
-            List<PMIB_IPFORWARDROW> filtered = new List<PMIB_IPFORWARDROW>();
+            List<Native.MIB_IPFORWARDROW> filtered = new List<Native.MIB_IPFORWARDROW>();
             for (int i = 0; i < forwardTable.Table.Length; ++i)
             {
                 if (Convert.ToInt32(forwardTable.Table[i].dwForwardIfIndex).Equals(interfaceIndex))
@@ -289,13 +246,13 @@ namespace NetworkRoute
                 }
             }
 
-            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(PMIB_IPFORWARDROW)));
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(Native.MIB_IPFORWARDROW)));
             try
             {
-                foreach (PMIB_IPFORWARDROW routeEntry in filtered)
+                foreach (Native.MIB_IPFORWARDROW routeEntry in filtered)
                 {
                     Marshal.StructureToPtr(routeEntry, ptr, false);
-                    var status = NativeMethods.DeleteIpForwardEntry(ptr);
+                    var status = Native.DeleteIpForwardEntry(ptr);
                 }
             }
             finally
@@ -319,8 +276,89 @@ namespace NetworkRoute
         public int Metric { get; set; }
     }
 
-    internal static class NativeMethods
+    internal static class Native
     {
+        //https://docs.microsoft.com/en-us/windows/win32/iphlp/managing-routing
+        //https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-rrasm/5dca234b-bea4-4e67-958e-5459a32a7b71
+
+
+        [ComVisible(false), StructLayout(LayoutKind.Sequential)]
+        public struct IPForwardTable
+        {
+            public uint Size;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)]
+            public MIB_IPFORWARDROW[] Table;
+        };
+
+        [ComVisible(false), StructLayout(LayoutKind.Sequential)]
+        public struct MIB_IPFORWARDROW
+        {
+            internal uint /*DWORD*/ dwForwardDest;
+            internal uint /*DWORD*/ dwForwardMask;
+            internal uint /*DWORD*/ dwForwardPolicy;
+            internal uint /*DWORD*/ dwForwardNextHop;
+            internal uint /*DWORD*/ dwForwardIfIndex;
+            internal uint /*DWORD*/ dwForwardType;
+            internal uint /*DWORD*/ dwForwardProto;
+            internal uint /*DWORD*/ dwForwardAge;
+            internal uint /*DWORD*/ dwForwardNextHopAS;
+            internal uint /*DWORD*/ dwForwardMetric1;
+            internal uint /*DWORD*/ dwForwardMetric2;
+            internal uint /*DWORD*/ dwForwardMetric3;
+            internal uint /*DWORD*/ dwForwardMetric4;
+            internal uint /*DWORD*/ dwForwardMetric5;
+        };
+
+        public enum MIB_IPFORWARD_TYPE
+        {
+            MIB_IPROUTE_TYPE_OTHER = 1,
+            MIB_IPROUTE_TYPE_INVALID = 2,
+            MIB_IPROUTE_TYPE_DIRECT = 3,
+            MIB_IPROUTE_TYPE_INDIRECT = 4
+        }
+
+
+
+        public enum MIB_IPFORWARD_PROTO
+        {
+            MIB_IPPROTO_OTHER = 1,
+            MIB_IPPROTO_LOCAL = 2,
+            MIB_IPPROTO_NETMGMT = 3,
+            MIB_IPPROTO_ICMP = 4,
+            MIB_IPPROTO_EGP = 5,
+            MIB_IPPROTO_GGP = 6,
+            MIB_IPPROTO_HELLO = 7,
+            MIB_IPPROTO_RIP = 8,
+            MIB_IPPROTO_IS_IS = 9,
+            MIB_IPPROTO_ES_IS = 10,
+            MIB_IPPROTO_CISCO = 11,
+            MIB_IPPROTO_BBN = 12,
+            MIB_IPPROTO_OSPF = 13,
+            MIB_IPPROTO_BGP = 14,
+            MIB_IPPROTO_NT_AUTOSTATIC = 10002,
+            MIB_IPPROTO_NT_STATIC = 10006,
+            MIB_IPPROTO_NT_STATIC_NON_DOD = 10007
+        }
+
+
+        public static IPForwardTable ReadIPForwardTable(IntPtr tablePtr)
+        {
+            var result = (IPForwardTable)Marshal.PtrToStructure(tablePtr, typeof(IPForwardTable));
+
+            MIB_IPFORWARDROW[] table = new MIB_IPFORWARDROW[result.Size];
+            IntPtr p = new IntPtr(tablePtr.ToInt64() + Marshal.SizeOf(result.Size));
+            for (int i = 0; i < result.Size; ++i)
+            {
+                table[i] = (MIB_IPFORWARDROW)Marshal.PtrToStructure(p, typeof(MIB_IPFORWARDROW));
+                p = new IntPtr(p.ToInt64() + Marshal.SizeOf(typeof(MIB_IPFORWARDROW)));
+            }
+            result.Table = table;
+
+            return result;
+        }
+
+
         [DllImport("iphlpapi", CharSet = CharSet.Auto)]
         public extern static int GetIpForwardTable(IntPtr /*PMIB_IPFORWARDTABLE*/ pIpForwardTable, ref int /*PULONG*/ pdwSize, bool bOrder);
 
