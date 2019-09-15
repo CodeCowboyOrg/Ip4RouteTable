26f21028103c61c3c61bfd853c90d03f2defad1a
CodeCowboyOrg <CodeCowboyOrg@gmail.com>
2019-09-15 17:01:04 -0400

    Added Nuget Spec file

--- Networking/Properties/AssemblyInfo.cs
+++ Networking/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CodeCowboy.NetworkRoute")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Network routing table interactions")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("www.CodeCowboy.org")]
 [assembly: AssemblyProduct("CodeCowboy.NetworkRoute")]
